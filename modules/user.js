2afe06acdbc4c05e0beded0177ee0eac7a6b6113
emilyl <emily.lukas@gmail.com>
2021-05-22 15:55:51 -0700

    update request params

--- modules/user.js
+++ modules/user.js
@@ -24,10 +24,9 @@ const fetch = async (params) => {
 
 const update = async (params) => {
   try {
-    let user = await User.findOne({ name: params.name });
+    let user = await User.findOne({ name: params.body.name });
     if (user === null) throw "User Not Found";
-
-    await User.findOneAndUpdate({ name: params.name }, params);
+    await User.findOneAndUpdate({ name: params.body.name }, params);
     return { message: "Success" };
   } catch (err) {
     return { error: err, message: "User Not Found" };
