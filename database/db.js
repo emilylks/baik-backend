f4da8b30495e5ead3ce9efbad8a5204494a8eb21
emilylks <emily.lukas@gmail.com>
2021-05-23 00:00:02 +0000

    commit to merge

--- database/db.js
+++ database/db.js
@@ -8,6 +8,7 @@ mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 
 //Get the default connection
 var db = mongoose.connection;
+console.log(db);
 
 //Bind connection to error event (to get notification of connection errors)
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
@@ -18,13 +19,13 @@ const userSchema = new Schema({
   checkIns: [{ sleepHours: Number, sleepQuality: Number, mood: Number, date: Date }],
 });
 
-/*userSchema.set('toJSON', {
+userSchema.set('toJSON', {
     virtuals: true,
     versionKey: false,
     transform: function (doc, ret) {
         delete ret._id;
         delete ret.hash;
     }
-});*/
+});
 
 module.exports = mongoose.model('User', userSchema);
