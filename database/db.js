1bcfe42e7a2c40101b14628c64b7194b4b2866e9
emilyl <emily.lukas@gmail.com>
2021-05-22 20:52:19 -0700

    change db again

--- database/db.js
+++ database/db.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 
 //Set up default mongoose connection
 var mongoDB = 'mongodb://127.0.0.1:27017/baik';
-mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
 
 //Get the default connection
 var db = mongoose.connection;
