3e7b37b819f748ed5547d7ec07dd2de3ebc71847
emilyl <emily.lukas@gmail.com>
2021-05-22 21:25:49 -0700

    move headers block

--- index.js
+++ index.js
@@ -9,8 +9,15 @@ app.use(bodyParser.json())
 // parse requests of content-type: application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
-app.use('/user', userRouter);
 
+app.use((req, res, next) => {
+    res.append('Access-Control-Allow-Origin', ['*']);
+    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.append('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
+app.use('/user', userRouter);
 app.get("/", (req, res) => {
   res.json({ message: "Hello World" });
 });
@@ -19,11 +26,5 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
-app.use((req, res, next) => {
-    res.append('Access-Control-Allow-Origin', ['*']);
-    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.append('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-});
 
 app.listen(port, () => console.log("Listening on port 3000"));
